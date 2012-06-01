bc09a496e73c9a80670ab45b415dbc30ad361f69
kapilratnani <kapil.ratnani@iiitb.net>
2012-06-01 16:54:34 +0530

    coin arrangement, made shooting line, used polar equation of line

---
+++ Carrom/src/net/ripper/carrom/model/components/PolarLine.java
@@ -0,0 +1,52 @@
+package net.ripper.carrom.model.components;
+
+/**
+ * line with polar coordinates
+ * 
+ * @author theripper
+ * 
+ */
+public class PolarLine {
+	public float originX;
+	public float originY;
+	public float r;
+	public float theta;
+	private float finalX;
+	private float finalY;
+
+	/**
+	 * 
+	 * @param originX
+	 * @param originY
+	 * @param r
+	 * @param theta
+	 *            in radians
+	 */
+	public PolarLine(float originX, float originY, float r, float theta) {
+		this.originX = originX;
+		this.originY = originY;
+		this.r = r;
+		this.theta = theta;
+		this.finalX = (float) (originX + r * Math.cos(theta));
+		this.finalY = (float) (originY + r * Math.sin(theta));
+	}
+
+	public float getFinalX() {
+		return finalX;
+	}
+
+	public float getFinalY() {
+		return finalY;
+	}
+
+	/**
+	 * 
+	 * @param theta
+	 *            in radians
+	 */
+	public void rotateTo(float theta) {
+		this.theta = theta;
+		this.finalX = (float) (originX + r * Math.cos(theta));
+		this.finalY = (float) (originY + r * Math.sin(theta));
+	}
+}
